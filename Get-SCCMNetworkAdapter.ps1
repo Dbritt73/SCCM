70bebb3602b806e61a510b64e23996e1b3a58069
Darrin Britton <brittod@wwu.edu>
2019-03-14 14:56:32 -0700

    Added help, formatting

--- Get-SCCMNetworkAdapter.ps1
+++ Get-SCCMNetworkAdapter.ps1
@@ -1,11 +1,42 @@
 Function Get-SCCMNetworkAdapter {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Get-SCCMNetworkAdapter" in 1-2 sentences.
+
+    .DESCRIPTION
+    Add a more complete description of what the function does.
+
+    .PARAMETER ComputerName
+    Describe parameter -ComputerName.
+
+    .PARAMETER ReportUri
+    Describe parameter -ReportUri.
+
+    .EXAMPLE
+    Get-SCCMNetworkAdapter -ComputerName Value -ReportUri Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Get-SCCMNetworkAdapter
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
     [CmdletBinding()]
     Param (
 
-        [Parameter( Mandatory=$true,
-                    ValueFromPipeline=$True,
-                    ValueFromPipelineByPropertyName=$true,
-                    Position=0)]
+        [Parameter( Mandatory = $true,
+                    HelpMessage = 'Name of computer(s) to query',
+                    ValueFromPipeline = $True,
+                    ValueFromPipelineByPropertyName = $true,
+                    Position = 0)]
         [String[]]$ComputerName,
 
         [String]$ReportUri = 'http://Report-Server_URI-Goes_HERE%2fHardware+-+Network+Adapter%2fNetwork+adapter+information+for+a+specific+computer&rs:format=xml&variable='
@@ -21,10 +52,19 @@ Function Get-SCCMNetworkAdapter {
             Foreach ($Computer in $ComputerName) {
 
                 $ReportServerUri = $ReportUri + $Computer
-                $Response = invoke-RestMethod -Uri $ReportServerUri -UseDefaultCredential
+
+                $splat = @{
+
+                    'Uri'                   = $ReportServerUri
+                    'UseDefaultCredentials' = $true
+                    'ErrorAction'           = 'Stop'
+
+                }
+
+                $Response = invoke-RestMethod @splat
 
                 #Regex required to scrub the XML string of any non-standard ASCII chars
-                [xml]$XMLReport = $Response -replace "[^ -x7e]",""
+                [xml]$XMLReport = $Response -replace '[^ -x7e]',''
 
                 foreach ($NetAdapter in $XMLReport.Report.Table0.Detail_Collection.Detail) {
 
@@ -41,14 +81,34 @@ Function Get-SCCMNetworkAdapter {
                     }
 
                     $Object = New-object -typename PSObject -Property $Properties
-                    $Object.PSObject.TypeNames.Insert(0,'SCCM.Desktop')
-                    Write-Output $Object
+                    $Object.PSObject.TypeNames.Insert(0,'Report.SCCM.NetworkAdapter')
+                    Write-Output -InputObject $Object
 
                 }
 
             }
 
-        } Catch {}
+        } Catch {
+
+            # get error record
+            [Management.Automation.ErrorRecord]$e = $_
+
+            # retrieve information about runtime error
+            $info = [PSCustomObject]@{
+
+                Exception = $e.Exception.Message
+                Reason    = $e.CategoryInfo.Reason
+                Target    = $e.CategoryInfo.TargetName
+                Script    = $e.InvocationInfo.ScriptName
+                Line      = $e.InvocationInfo.ScriptLineNumber
+                Column    = $e.InvocationInfo.OffsetInLine
+
+            }
+
+            # output information. Post-process collected info, and log info (optional)
+            $info
+
+        }
 
     }
 
